7ecce6e24c194271521e54c30e2dc130c03a7197
Fabian Alexander Wilms <fabian-alexander.wilms@basf.com>
2022-12-10 00:21:36 +0100

    Day 6 Part 2 done

--- day_6__tuning_trouble.py
+++ day_6__tuning_trouble.py
@@ -1,26 +1,33 @@
-def get_first_marker_idx(buffer: str) -> int:
+from pprint import pprint
+
+
+def get_first_idx_after_n_distinct_chars(buffer: str, n: int) -> int:
     idx = 1
     for _ in buffer:
-        marker_found = True
-        if idx >= 4:
-            for i in range(4):
-                for j in range(4):
-                    if i is not j:
-                        if buffer[idx-1-i] == buffer[idx-1-j]:
-                            marker_found = False
-
-            if marker_found:
+        if idx >= n:
+            potential_marker = buffer[idx-n:idx]
+            unique_chars = set(potential_marker)
+            if len(potential_marker) == len(unique_chars):
                 return idx
         idx += 1
 
 
-print(str(get_first_marker_idx('mjqjpqmgbljsphdztnvjfqwrcgsmlb')))
-print(str(get_first_marker_idx('bvwbjplbgvbhsrlpgdmjqwftvncz')))
-print(str(get_first_marker_idx('nppdvjthqldpwncqszvftbrmjlhg')))
-print(str(get_first_marker_idx('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg')))
-print(str(get_first_marker_idx('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw')))
+pprint(get_first_idx_after_n_distinct_chars('mjqjpqmgbljsphdztnvjfqwrcgsmlb', 4))
+pprint(get_first_idx_after_n_distinct_chars('bvwbjplbgvbhsrlpgdmjqwftvncz', 4))
+pprint(get_first_idx_after_n_distinct_chars('nppdvjthqldpwncqszvftbrmjlhg', 4))
+pprint(get_first_idx_after_n_distinct_chars('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 4))
+pprint(get_first_idx_after_n_distinct_chars('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 4))
 
 with open('day_6_input.txt') as file:
     data = file.read()
 
-print(str(get_first_marker_idx(data)))
+solution_part_1 = get_first_idx_after_n_distinct_chars(data, 4)
+assert solution_part_1 == 1042
+pprint(solution_part_1)
+
+pprint(get_first_idx_after_n_distinct_chars('mjqjpqmgbljsphdztnvjfqwrcgsmlb', 14))
+pprint(get_first_idx_after_n_distinct_chars('bvwbjplbgvbhsrlpgdmjqwftvncz', 14))
+pprint(get_first_idx_after_n_distinct_chars('nppdvjthqldpwncqszvftbrmjlhg', 14))
+pprint(get_first_idx_after_n_distinct_chars('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 14))
+pprint(get_first_idx_after_n_distinct_chars('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 14))
+pprint(get_first_idx_after_n_distinct_chars(data, 14))
