94ebfe00e30e53491eaeb81dd3c10008ea12eca7
Fabian Alexander Wilms <fabian-alexander.wilms@basf.com>
2022-12-09 02:01:35 +0100

    Day 1 done

---
+++ day_1__calorie_counting.py
@@ -0,0 +1,32 @@
+from pprint import pprint
+
+calories_of_current_elf = 0
+calories_of_each_elf = []
+no_newline_before_eof = 0
+with open('day_1_input.txt') as f:
+    for line in f:
+        # print(line.strip())
+        if line == '\n':
+            calories_of_each_elf.append(calories_of_current_elf)
+            calories_of_current_elf = 0
+            no_newline_before_eof = 0
+        else:
+            calories_of_current_elf += int(line)
+            no_newline_before_eof = 1
+
+if no_newline_before_eof:
+    calories_of_each_elf.append(calories_of_current_elf)
+
+# pprint(calories_of_each_elf)
+
+max_value = max(calories_of_each_elf)
+print(max_value)
+
+max_value_top_3 = 0
+for _ in range(1,3+1):
+    max_value = max(calories_of_each_elf)
+    max_value_top_3 += max_value
+    max_idx = calories_of_each_elf.index(max_value)
+    calories_of_each_elf[max_idx] = 0
+
+print(max_value_top_3)
