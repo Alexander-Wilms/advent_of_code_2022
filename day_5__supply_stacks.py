3e583d6120a02c97a9607f2b95dcc334178c56da
Fabian Alexander Wilms <fabian-alexander.wilms@basf.com>
2022-12-09 20:57:11 +0100

    Day 5 Part 2 done

--- day_5__supply_stacks.py
+++ day_5__supply_stacks.py
@@ -1,14 +1,24 @@
 import re
 from pprint import pprint
+from copy import deepcopy
 
 
-def print_crates(crates):
+def print_crates(crates_to_print):
     max_stack_size = 0
     stack_no = 1
-    for stack in crates:
+    for stack in crates_to_print:
         max_stack_size = max(max_stack_size, len(stack))
         print(str(stack_no)+': '+' '.join(stack[::-1]))
         stack_no += 1
+
+
+def print_top_crates(part, crates_to_print):
+    print_crates(crates_to_print)
+    message = ''
+    for stack in crates_to_print:
+        top_crate = stack[0][1]
+        message += top_crate
+    print('solution to part '+str(part)+': '+message)
 
 
 crates = []
@@ -42,8 +52,12 @@ with open('day_5_input.txt') as f:
 pprint(crates)
 pprint(moves)
 
-print_crates(crates)
 
+# https://stackoverflow.com/a/42449199/2278742
+crates_part_2 = deepcopy(crates)
+
+print('crates for part 1 before moving:')
+print_crates(crates)
 
 for move in moves:
 
@@ -57,21 +71,39 @@ for move in moves:
     number_of_crates_on_to_stack = len(crates[to_stack])
 
     move_str = 'move '+str(number_of_crates)+' from '+str(from_stack+1)+' to '+str(to_stack+1)
-
     print(move_str)
 
     for idx in range(number_of_crates):
-
         crate = crates[from_stack].pop(0)
         crates[to_stack].insert(0, crate)
 
     assert number_of_crates_on_from_stack - number_of_crates == len(crates[from_stack])
     assert number_of_crates_on_to_stack + number_of_crates == len(crates[to_stack])
 
+print('crates for part 1 before moving:')
+print_crates(crates)
 
-message = ''
-for stack in crates:
-    top_crate = stack[0][1]
-    message += top_crate
+for move in moves:
+    number_of_crates = move[0]
+    from_stack = move[1]-1
+    to_stack = move[2]-1
 
-print(message)
+    print_crates(crates_part_2)
+
+    move_str = 'move '+str(number_of_crates)+' from '+str(from_stack+1)+' to '+str(to_stack+1)
+    print(move_str)
+
+    crates_on_crane = []
+    for idx in range(number_of_crates):
+        crates_on_crane.append(crates_part_2[from_stack].pop(0))
+
+    print('crates on crane:')
+    pprint(crates_on_crane)
+
+    crates_on_crane.reverse()
+
+    for crate in crates_on_crane:
+        crates_part_2[to_stack].insert(0, crate)
+
+print_top_crates(1, crates)
+print_top_crates(2, crates_part_2)
