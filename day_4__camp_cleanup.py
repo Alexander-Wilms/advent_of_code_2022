165aa89eb4980ea787e9e6abe3b3a64aab370f6b
Fabian Alexander Wilms <fabian-alexander.wilms@basf.com>
2022-12-09 16:14:56 +0100

    Day 4 Part 1 done

---
+++ day_4__camp_cleanup.py
@@ -0,0 +1,19 @@
+from pprint import pprint
+
+fully_contained_pairs = 0
+with open('day_4_input.txt') as f:
+    for line in f:
+        assignments_raw = line.strip()
+        assignments = assignments_raw.split(',')
+        indices = []
+        for assignment in assignments:
+            indices_raw = assignment.split('-')
+            indices.append(int(indices_raw[0]))
+            indices.append(int(indices_raw[1]))
+        pprint(indices)
+
+        if (indices[0] <= indices[2] and indices[1] >= indices[3]) or \
+                ((indices[2] <= indices[0] and indices[3] >= indices[1])):
+            fully_contained_pairs += 1
+
+print('solution for part 1: '+str(fully_contained_pairs))
