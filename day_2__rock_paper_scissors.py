915ef9d71068d6e96de7807d3f3159d81e9bfe70
Fabian Alexander Wilms <fabian-alexander.wilms@basf.com>
2022-12-09 02:38:22 +0100

    Day 2 Part 2 done

--- day_2__rock_paper_scissors.py
+++ day_2__rock_paper_scissors.py
@@ -20,29 +20,78 @@ def get_outcome_of_round(opponent: RPS, myself: RPS) -> Outcome:
             (opponent == RPS.paper and myself == RPS.rock):
         return Outcome.lost
     else:
-        return Outcome(6-get_outcome_of_round(myself, opponent).value)
+        inverted_outcome = get_outcome_of_round(myself, opponent)
+        pprint(inverted_outcome)
+        print(6-inverted_outcome.value)
+        return Outcome(6-inverted_outcome.value)
 
 
 def get_score_of_round(opponent: RPS, myself: RPS):
+    pprint(opponent)
+    pprint(myself)
     return myself.value + get_outcome_of_round(opponent, myself).value
 
 
 def get_RPS_from_strategy_guide(line):
     rps = line.split()
     rps_enum = []
-    for i in range(2):
-        rps_enum.append(map_strategy_guide_to_enum(rps[i]))
+    opponent = map_strategy_guide_opponent_to_enum(rps[0])
+    rps_enum.append(opponent)
+    myself = map_strategy_guide_myself_to_Outcome_enum(rps[1])
+    rps_enum.append(map_strategy_guide_myself_to_RPS_enum(opponent, myself))
     return rps_enum
 
 
-def map_strategy_guide_to_enum(character) -> RPS:
+def map_strategy_guide_opponent_to_enum(character) -> RPS:
     match character:
-        case 'A' | 'X':
+        case 'A':
             return RPS.rock
-        case 'B' | 'Y':
+        case 'B':
             return RPS.paper
-        case 'C' | 'Z':
+        case 'C':
             return RPS.scissors
+
+
+def map_strategy_guide_myself_to_Outcome_enum(myself: str) -> Outcome:
+    match myself:
+        case 'X':
+            return Outcome.lost
+        case 'Y':
+            return Outcome.draw
+        case 'Z':
+            return Outcome.won
+
+
+def map_strategy_guide_myself_to_RPS_enum(opponent: RPS, myself: Outcome) -> RPS:
+    match myself:
+        case Outcome.draw:
+            return opponent
+        case Outcome.lost:
+            return get_myself_lost(opponent)
+        case Outcome.won:
+            return get_myself_won(opponent)
+        case _:
+            raise ValueError("Could not match "+str(opponent)+' and '+str(myself))
+
+
+def get_myself_lost(opponent: RPS) -> RPS:
+    match opponent:
+        case RPS.rock:
+            return RPS.scissors
+        case RPS.paper:
+            return RPS.rock
+        case RPS.scissors:
+            return RPS.paper
+
+
+def get_myself_won(opponent: RPS) -> RPS:
+    match opponent:
+        case RPS.rock:
+            return RPS.paper
+        case RPS.paper:
+            return RPS.scissors
+        case RPS.scissors:
+            return RPS.rock
 
 
 total_score = 0
