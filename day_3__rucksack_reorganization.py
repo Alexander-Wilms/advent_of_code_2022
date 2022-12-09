ad9d2575cf08370e9c124f04f2e7067a7f3c1869
Fabian Alexander Wilms <fabian-alexander.wilms@basf.com>
2022-12-09 15:53:16 +0100

    Day 3 Part 2 done

--- day_3__rucksack_reorganization.py
+++ day_3__rucksack_reorganization.py
@@ -1,3 +1,6 @@
+from pprint import pprint
+
+
 def map_item_to_priority(item: str):
     ascii_value = ord(item)
     if ord('a') <= ascii_value <= ord('z'):
@@ -29,4 +32,40 @@ with open('day_3_input.txt') as f:
         print(duplicate_item)
         sum_of_priorities += map_item_to_priority(duplicate_item)
 
-print(sum_of_priorities)
+print('answer for part 1: '+str(sum_of_priorities))
+
+lines = []
+with open('day_3_input.txt') as f:
+    for line in f:
+        lines.append(line.strip())
+
+pprint(lines)
+
+sum_of_priorities = 0
+done_with_group = False
+for line_idx in range(0, len(lines), 3):
+    print(line_idx)
+
+    first_line = lines[line_idx]
+    second_line = lines[line_idx+1]
+    third_line = lines[line_idx+2]
+
+    print(first_line)
+    print(second_line)
+    print(third_line)
+
+    done_with_group = False
+
+    for char_first_line in first_line:
+        for char_second_line in second_line:
+            for char_third_line in third_line:
+                if char_first_line == char_second_line == char_third_line:
+                    if not done_with_group:
+                        print(char_first_line)
+                        sum_of_priorities += map_item_to_priority(char_first_line)
+                        done_with_group = True
+                        break
+
+    print('---')
+
+print('answer for part 2: '+str(sum_of_priorities))
