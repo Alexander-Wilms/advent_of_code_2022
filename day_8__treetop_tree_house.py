31bcf655dd78dc1e6632a7286cebf6e41287f4df
Fabian Alexander Wilms <fabian-alexander.wilms@basf.com>
2022-12-12 00:16:39 +0100

    Day 8 Part 1 done

---
+++ day_8__treetop_tree_house.py
@@ -0,0 +1,118 @@
+import os
+from pprint import pprint
+
+import numpy
+from colorama import Back
+
+
+def print_map(tree_map: numpy.ndarray, highlight: bool = False, highlight_row: int = -1, highlight_col: int = -1):
+    (rows, cols) = tree_map.shape
+    for row in range(rows):
+        for col in range(cols):
+            tree = str(tree_map[row, col])
+            if highlight and row == highlight_row and col == highlight_col:
+                tree = Back.RED + tree
+            print(Back.GREEN+tree+' '+Back.RESET, end='')
+        print('')
+
+
+def is_visible_from_direction(tree_map: numpy.ndarray, row, col, direction: str) -> bool:
+    print('is_visible_from_direction()')
+    height_of_tree_to_check = tree_map[row, col]
+
+    (rows, cols) = tree_map.shape
+    match direction:
+        case '→':
+            tree_map_aligned = tree_map
+            aligned_row = row
+            aligned_col = col
+        case '↑':
+            tree_map_aligned = numpy.rot90(tree_map, axes=(1, 0))
+            aligned_row = col
+            aligned_col = rows-row-1
+        case '←':
+            tree_map_aligned = numpy.rot90(tree_map, k=2, axes=(1, 0))
+            aligned_row = rows-row-1
+            aligned_col = cols-col-1
+        case '↓':
+            tree_map_aligned = numpy.rot90(tree_map, k=3, axes=(1, 0))
+            aligned_row = cols-col-1
+            aligned_col = row
+
+    print(direction)
+    #print_map(tree_map_aligned, True, aligned_row, aligned_col)
+
+    visible = True
+
+    print(height_of_tree_to_check)
+
+    for idx in range(aligned_col):
+        #print('checking col '+str(idx))
+        if tree_map_aligned[aligned_row, idx] >= height_of_tree_to_check:
+            visible = False
+
+    pprint(visible)
+    print('---')
+    return visible
+
+
+def is_visible(tree_map: numpy.ndarray, row, col) -> bool:
+    (rows, cols) = tree_map.shape
+    if row == 0 or col == 0 or row == rows-1 or col == cols-1:
+        # trees around the edge
+        return True
+    else:
+        visible = False
+        for direction in '→↑←↓':
+            visible |= is_visible_from_direction(tree_map, row, col, direction)
+        return visible
+
+
+# Fixes ANSI colors for some reason
+# https://stackoverflow.com/a/64222858/2278742
+os.system("")
+
+input_file = 'day_8_input.txt'
+
+height = 0
+line_length_counted = False
+with open(input_file) as file:
+    for line in file:
+        if not line_length_counted:
+            # don't count the newline
+            width = len(line)-1
+            line_length_counted = True
+        print(line.strip())
+        height += 1
+
+    print(str(width)+', '+str(height))
+
+    trees = numpy.zeros((width, height)).astype(int)
+
+    pprint(trees)
+
+with open(input_file) as file:
+    line_count = 0
+    for line in file:
+        char_count = 0
+        for char in line:
+            if char != '\n':
+                trees[line_count, char_count] = int(char)
+                char_count += 1
+        line_count += 1
+
+    pprint(trees)
+
+tree_visibility = numpy.full((width, height), False)
+
+is_visible(trees, 1, 2)
+
+for row in range(height):
+    for col in range(width):
+        tree_visibility[row, col] = is_visible(trees, row, col)
+
+print_map(trees)
+
+pprint(tree_visibility)
+
+print(tree_visibility.sum())
