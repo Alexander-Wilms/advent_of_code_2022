0b7f8d9ee57b87eb64e7283a73003c9ec675fa8b
Fabian Alexander Wilms <fabian-alexander.wilms@basf.com>
2022-12-12 01:41:51 +0100

    Day 8 Part 2 done

--- day_8__treetop_tree_house.py
+++ day_8__treetop_tree_house.py
@@ -16,43 +16,40 @@ def print_map(tree_map: numpy.ndarray, highlight: bool = False, highlight_row: i
         print('')
 
 
-def is_visible_from_direction(tree_map: numpy.ndarray, row, col, direction: str) -> bool:
-    print('is_visible_from_direction()')
-    height_of_tree_to_check = tree_map[row, col]
-
-    (rows, cols) = tree_map.shape
+def get_aligned_map(map: numpy.ndarray, row: int, col: int, direction: str) -> tuple[numpy.ndarray, int, int]:
+    (rows, cols) = map.shape
     match direction:
         case '→':
-            tree_map_aligned = tree_map
+            aligned_map = map
             aligned_row = row
             aligned_col = col
         case '↑':
-            tree_map_aligned = numpy.rot90(tree_map, axes=(1, 0))
+            aligned_map = numpy.rot90(map, axes=(1, 0))
             aligned_row = col
             aligned_col = rows-row-1
         case '←':
-            tree_map_aligned = numpy.rot90(tree_map, k=2, axes=(1, 0))
+            aligned_map = numpy.rot90(map, k=2, axes=(1, 0))
             aligned_row = rows-row-1
             aligned_col = cols-col-1
         case '↓':
-            tree_map_aligned = numpy.rot90(tree_map, k=3, axes=(1, 0))
+            aligned_map = numpy.rot90(map, k=3, axes=(1, 0))
             aligned_row = cols-col-1
             aligned_col = row
 
-    print(direction)
-    #print_map(tree_map_aligned, True, aligned_row, aligned_col)
+    return aligned_map, aligned_row, aligned_col
+
+
+def is_visible_from_direction(tree_map: numpy.ndarray, row, col, direction: str) -> bool:
+    height_of_tree_to_check = tree_map[row, col]
+
+    tree_map_aligned, aligned_row, aligned_col = get_aligned_map(tree_map, row, col, direction)
 
     visible = True
 
-    print(height_of_tree_to_check)
-
     for idx in range(aligned_col):
-        #print('checking col '+str(idx))
         if tree_map_aligned[aligned_row, idx] >= height_of_tree_to_check:
             visible = False
 
-    pprint(visible)
-    print('---')
     return visible
 
 
@@ -66,6 +63,24 @@ def is_visible(tree_map: numpy.ndarray, row, col) -> bool:
         for direction in '→↑←↓':
             visible |= is_visible_from_direction(tree_map, row, col, direction)
         return visible
+
+
+def get_scenic_score(map: numpy.ndarray, row: int, col: int) -> int:
+    (rows, cols) = map.shape
+    height_of_tree_to_check = map[row, col]
+    scenic_score = 1
+    for direction in '↑←→↓':
+        aligned_map, aligned_row, aligned_col = get_aligned_map(map, row, col, direction)
+
+        viewing_distance = 0
+        for idx in range(aligned_col+1, cols):
+            value = aligned_map[aligned_row, idx]
+            viewing_distance += 1
+            if value >= height_of_tree_to_check:
+                break
+
+        scenic_score *= viewing_distance
+    return scenic_score
 
 
 # Fixes ANSI colors for some reason
@@ -105,8 +120,6 @@ with open(input_file) as file:
 
 tree_visibility = numpy.full((width, height), False)
 
-is_visible(trees, 1, 2)
-
 for row in range(height):
     for col in range(width):
         tree_visibility[row, col] = is_visible(trees, row, col)
@@ -115,4 +128,13 @@ print_map(trees)
 
 pprint(tree_visibility)
 
-print(tree_visibility.sum())
+scenic_scores = numpy.zeros((width, height)).astype(int)
+
+for row in range(height):
+    for col in range(width):
+        scenic_scores[row, col] = get_scenic_score(trees, row, col)
+
+pprint(scenic_scores)
+
+print('solution to part 1: '+str(tree_visibility.sum()))
+print('solution to part 2: '+str(scenic_scores.max()))
