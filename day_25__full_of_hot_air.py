cfa03879654e7d90ef119c0b95043683810f56c0
Alexander Wilms <f.alexander.wilms@gmail.com>
2022-12-29 18:24:43 +0100

    Day 25 Part 1: One example works

--- day_25__full_of_hot_air.py
+++ day_25__full_of_hot_air.py
@@ -1,5 +1,8 @@
 from pprint import pprint
 
+from constraint import *
+from sympy import Abs, Symbol, nonlinsolve, solve, symbols
+
 
 def snafu_to_decimal(snafu: str) -> int:
     digits = {'2': 2, '1': 1, '0': 0, '-': -1, '=': -2}
@@ -18,3 +21,12 @@ with open('day_25_example.txt') as file:
 
 pprint(decimal_values)
 pprint(sum(decimal_values))
+
+# solution can be found usin WolframAlpha with the following system of equations and inequalities:
+# x3*5^3+x2*5^2+x1*5^1+x0*5^0=107, abs(x3)<=2, abs(x2)<=2, abs(x1)<=2, abs(x0)<=2
+
+# solution to a snafu example using constraint
+snafu_problem = Problem()
+snafu_problem.addVariables(['x3', 'x2', 'x1', 'x0'], [-2, -1, 0, 1, 2])
+snafu_problem.addConstraint(lambda x3, x2, x1, x0: x3*5**3+x2*5**2+x1*5**1+x0*5**0 == 107, ('x3', 'x2', 'x1', 'x0'))
+pprint(snafu_problem.getSolutions())
