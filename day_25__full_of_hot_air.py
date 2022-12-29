eb3e83caa76725fcab1dd10723ece3fc561c641c
Alexander Wilms <f.alexander.wilms@gmail.com>
2022-12-29 16:22:38 +0100

    Day 25 Part 1: Extract snafu to decimal conversion into function

--- day_25__full_of_hot_air.py
+++ day_25__full_of_hot_air.py
@@ -1,17 +1,20 @@
 from pprint import pprint
 
-digits = {'2': 2, '1': 1, '0': 0, '-': -1, '=': -2}
+
+def snafu_to_decimal(snafu: str) -> int:
+    digits = {'2': 2, '1': 1, '0': 0, '-': -1, '=': -2}
+    current_digit = len(snafu)-1
+    decimal_value = 0
+    for char in snafu:
+        decimal_value += digits[char]*pow(5, current_digit)
+        current_digit -= 1
+    return decimal_value
+
 
 decimal_values = []
 with open('day_25_example.txt') as file:
     for line in file:
-        stripped_line = line.strip()
-        current_digit = len(stripped_line)-1
-        decimal_value = 0
-        for char in stripped_line:
-            decimal_value += digits[char]*pow(5, current_digit)
-            current_digit -= 1
-        decimal_values.append(decimal_value)
+        decimal_values.append(snafu_to_decimal(line.strip()))
 
 pprint(decimal_values)
 pprint(sum(decimal_values))
